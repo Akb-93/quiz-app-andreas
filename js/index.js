954208a1b684fa27d110f8321752bfcf51a97f62
Andreas Buchberger <Buchberger.Andreas@icloud.com>
2025-03-14 16:31:47 +0100

    beer o-clock

--- js/index.js
+++ js/index.js
@@ -1,12 +1,21 @@
 const card = document.querySelector('[data-js="card"]');
 const answerTag = document.querySelector('[data-js="card-answer-tag"]');
 const answerButton = document.querySelector('[data-js="card-button"]');
-const cardBookmark = document.querySelector('[data-js="card-bookmark"]');
+const cardBookmark = document.querySelector('[data-js="toggle-bookmark"]');
 
 answerButton.addEventListener("click", () => {
   if (answerTag.style.display === "none") {
     answerTag.style.display = "block";
   } else {
     answerTag.style.display = "none";
+  }
+});
+
+cardBookmark.addEventListener("click", () => {
+  console.log("fail");
+  if (cardBookmark.style.backgroundColor == "#e1b94a") {
+    cardBookmark.style.backgroundColor = "#7d5fff";
+  } else {
+    cardBookmark.style.backgroundColor = "#e1b94a";
   }
 });
